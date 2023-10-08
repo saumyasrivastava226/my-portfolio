fb417584c3ce12095ad1456016b402af5f2db1e6
saumya <saumya.srivastava@elhi.tech>
2023-10-08 15:05:19 +0530

    changes in nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, makeStyles } from '@mui/material';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { Paper } from "@mui/material";
+import { Modal, Button } from "@mui/material";
+import Typography from "@material-ui/core/Typography";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    backgroundColor: '#2196F3', // Customize the background color
+    backgroundColor: "#2196F3",
   },
   title: {
     flexGrow: 1,
   },
   navLink: {
-    color: 'white', // Customize the link color
-    textDecoration: 'none',
-    marginLeft: theme.spacing(2), // Add some spacing between links
+    color: "white",
+    textDecoration: "none",
+    marginLeft: theme.spacing(2),
   },
   activeNavLink: {
-    fontWeight: 'bold', // Customize the active link style
+    fontWeight: "bold",
+  },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent:"right",
+    backgroundColor:"yellow",
+    
+   
+  },
+  headcontainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 }));
 
@@ -25,41 +48,47 @@ export function Header() {
   return (
     <AppBar position="static" className={classes.appBar}>
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          Your Name
-        </Typography>
-        <nav>
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                exact
-                className={classes.navLink}
-                activeClassName={classes.activeNavLink}
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/projects"
-                className={classes.navLink}
-                activeClassName={classes.activeNavLink}
-              >
-                Projects
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/contact"
-                className={classes.navLink}
-                activeClassName={classes.activeNavLink}
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+        <div className={classes.headcontainer}>
+          <div>
+            <Typography variant="h6" className={classes.title}>
+              Saumya Srivastava
+            </Typography>
+          </div>
+          <div className={classes.container}>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink
+                    to="/"
+                    exact
+                    className={classes.navLink}
+                    activeClassName={classes.activeNavLink}
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/projects"
+                    className={classes.navLink}
+                    activeClassName={classes.activeNavLink}
+                  >
+                    Projects
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/contact"
+                    className={classes.navLink}
+                    activeClassName={classes.activeNavLink}
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </Toolbar>
     </AppBar>
   );
